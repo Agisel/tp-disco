f17844ef23d0ff3e8abefcafb546e9b6ae617933
Agisel <abracaiteg@gmail.com>
2017-04-23 20:57:04 -0300

    commit clase

---
+++ src/disco1/Disco.java
@@ -0,0 +1,38 @@
+package disco1;
+
+public class Disco {
+	//atributos
+	private Double radioInterior;
+	private Double radioExterior;
+	
+	//constructor, que es lo que se va a cambiar en el test, y pide que se pueda cambiar el radio int y ext
+	public Disco(Double radioInterior,Double radioExterior){
+	this.radioInterior= radioInterior;
+	this.radioExterior= radioExterior;
+	}
+	
+	//metodos
+	
+
+	public Double calculaPerimetroInterior(){
+		Double perimetroInterior;
+		perimetroInterior= 2*Math.PI*radioInterior;
+		return perimetroInterior;
+	}
+	
+	public Double calculaPerimetroExterior(){
+		Double perimetroExterior;
+		perimetroExterior= 2*Math.PI*radioExterior;
+		return perimetroExterior;
+	}
+	
+	public Double calcularSuperficie(){
+		Double superficie; 
+		superficie= Math.PI*radioExterior*radioExterior;
+		return superficie;
+	}
+	
+}
+
+
+
